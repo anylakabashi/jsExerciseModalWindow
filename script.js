a18035e68b6e11ffb398f802885d0ee321766b16
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-17 18:50:59 +0100

    Variables for HTML elements

---
+++ script.js
@@ -0,0 +1,8 @@
+'strict mode'
+
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector('.close-modal');
+const showModal = document.querySelectorAll('show-modal')
+const overlay = document.querySelector(".overlay")
+
+console.log(modal, closeModal, showModal, overlay)
