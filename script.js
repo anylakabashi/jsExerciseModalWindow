2c44dda7da3391bb71cf6fa9a8f28ce786da48cb
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-17 19:36:56 +0100

    Made functional close button

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const overlay = document.querySelector(".overlay")
 
 const btnShowModal = document.querySelectorAll('.show-modal')
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  btnShowModal[i].addEventListener('click', function () {
-    overlay.style.display = "block";
-    modal.style.display = "block";
-  })
+const openModal = function () {
+  overlay.classList.remove('hidden')
+  modal.classList.remove('hidden')
 }
+const closeModal = function () {
+  overlay.classList.add("hidden")
+  modal.classList.add("hidden")
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal)
+}
+btnCloseModal.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
