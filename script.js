4415a3af82c2e7e6b194eee13bc7447030d68f79
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-17 20:31:13 +0100

    Made functional the esc key

--- script.js
+++ script.js
@@ -23,3 +23,8 @@ btnCloseModal.addEventListener('click', closeModal)
 
 overlay.addEventListener('click', closeModal)
 
+document.addEventListener('keydown', function (e) {
+  if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
