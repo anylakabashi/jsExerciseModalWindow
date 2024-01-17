3e787f59b984d08561e3182a71720835f90cd6b0
Anyla Kabashi <anyla.kabashi@gmail.com>
2024-01-17 19:15:01 +0100

    Made functional show button

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 'strict mode'
 
 const modal = document.querySelector(".modal")
-const closeModal = document.querySelector('.close-modal');
-const showModal = document.querySelectorAll('show-modal')
+const btnCloseModal = document.querySelector('.close-modal');
+
 const overlay = document.querySelector(".overlay")
 
-console.log(modal, closeModal, showModal, overlay)
+const btnShowModal = document.querySelectorAll('.show-modal')
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', function () {
+    overlay.style.display = "block";
+    modal.style.display = "block";
+  })
+}
